37f9d3e61741e7f3c5f8a30db51db6681fa92ace
Carlos Fdez. Llamas <hello@sirikon.me>
2023-04-09 19:58:29 +0200

    rename

--- src/lib/container/models.ts
+++ src/lib/container/models.ts
@@ -1,13 +1,13 @@
 // deno-lint-ignore-file no-explicit-any
 
 export type Clazz<T = any> = new (...args: any) => T;
-export type Indices<T> = Exclude<keyof T, keyof any[]>;
+export type ArrayIndexes<T> = Exclude<keyof T, keyof any[]>;
 
 export type Dependencies<T extends Clazz> =
   ConstructorParameters<T>["length"] extends 0 ? []
     : 
       & {
-        [P in Indices<ConstructorParameters<T>>]: Clazz<
+        [P in ArrayIndexes<ConstructorParameters<T>>]: Clazz<
           ConstructorParameters<T>[P]
         >;
       }
